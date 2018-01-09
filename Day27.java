214d858e49e54c5b0c96e8c97d8c8ab124d12e45
Jordan Hudson <hudsonjordan86@gmail.com>
2018-01-09 12:15:08 -0800

    Create Day27.java

---
+++ Day27.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+  
+      System.out.println("5");
+      System.out.println("4 3");
+      System.out.println("-1 0 4 2");
+      System.out.println("5 2");
+      System.out.println("0 -1 2 1 4");
+      System.out.println("8 4");
+      System.out.println("0 -2 -5 1 3 5 7 9");
+      System.out.println("7 3");
+      System.out.println("0 -1 -3 5 6 3 1");
+      System.out.println("6 4");
+      System.out.println("-5 -3 0 3 5 4");
+      
+      /* Yields the following output:
+         YES
+         NO
+         YES
+         NO
+         YES
+       */
+           
+    }
+}
